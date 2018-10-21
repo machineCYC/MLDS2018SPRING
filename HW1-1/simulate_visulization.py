253af6cbc8984905162eb2e8e8c4a84f829ddde1
CYC <yenchen0416@gmail.com>
2018-10-21 15:12:43 +0800

    modify colar, yscale

--- HW1-1/simulate_visulization.py
+++ HW1-1/simulate_visulization.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 
 def main(args):
     ### plot predict
-    # target function sin(5x*pi) / 5x*pi
+    ### target function sin(5x*pi) / 5x*pi
     X = np.linspace(0.0001, 1.0, num=1000)[:, np.newaxis]
     Y_true = np.sinc(5 * X)
     Y_shallow = np.load(os.path.join(args.LOG_DIR_PATH, "shallow", "predict_shallow.npy"))
@@ -18,12 +18,12 @@ def main(args):
     plt.title("Predict")
     plt.xlabel("X")
     plt.ylabel("Y")
-    plt.plot(X, Y_true, label=r"\mathrm{sin}(2 \omega t)$")
-    plt.plot(X, Y_shallow, label="shallow")
-    plt.plot(X, Y_medium, label="medium")
-    plt.plot(X, Y_deep, label="deep")
+    plt.plot(X, Y_true, c="black", label=r"$\mathrm{sin}(5 \pi x)/5x$")
+    plt.plot(X, Y_shallow, c="red", label="shallow")
+    plt.plot(X, Y_medium, c="green", label="medium")
+    plt.plot(X, Y_deep, c="blue", label="deep")
     plt.legend()
-    plt.show()
+    plt.savefig(os.path.join(args.SAVE_IMAGE_DIR_PATH, "predict.png"))
     plt.clf()
 
     ### plot loss
@@ -35,17 +35,19 @@ def main(args):
     plt.title("Loss v.s Epochs")
     plt.xlabel("# of epochs")
     plt.ylabel("Loss")
-    plt.plot(np.arange(len(shallow_loss)), shallow_loss, label="shallow")
-    plt.plot(np.arange(len(medium_loss)), medium_loss, label="medium")
-    plt.plot(np.arange(len(deep_loss)), deep_loss, label="deep")
+    plt.yscale("log")
+    plt.plot(np.arange(len(shallow_loss)), shallow_loss, c="red", label="shallow")
+    plt.plot(np.arange(len(medium_loss)), medium_loss, c="green", label="medium")
+    plt.plot(np.arange(len(deep_loss)), deep_loss, c="blue", label="deep")
     plt.legend()
-    plt.show()
+    plt.savefig(os.path.join(args.SAVE_IMAGE_DIR_PATH, "loss.png"))
     plt.clf()
 
 if __name__ == "__main__":
     PROJECT_DIR_PATH = os.path.dirname(__file__)
     LOG_DIR_PATH = os.path.join(PROJECT_DIR_PATH, "logs")
     SAVE_MODLE_DIR_PATH = os.path.join(PROJECT_DIR_PATH, "save_models")
+    SAVE_IMAGE_DIR_PATH = os.path.join(PROJECT_DIR_PATH, "image")
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -58,6 +60,12 @@ if __name__ == "__main__":
         "--SAVE_MODLE_DIR_PATH",
         type=str,
         default=SAVE_MODLE_DIR_PATH,
+        help=""
+    )
+    parser.add_argument(
+        "--SAVE_IMAGE_DIR_PATH",
+        type=str,
+        default=SAVE_IMAGE_DIR_PATH,
         help=""
     )
     main(parser.parse_args())
