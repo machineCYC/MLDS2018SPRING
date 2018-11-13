56be6889cbcd21dc33da82fe1c1fe74f2d13d4c4
CYC <yenchen0416@gmail.com>
2018-11-13 23:06:05 +0800

    add random label images

--- HW1-3/random_label.py
+++ HW1-3/random_label.py
@@ -11,9 +11,9 @@ from model.basic_model import simple_model
 def main(args):
     # parameters
     batch_size = 128
-    epochs = 2
-    learning_rate = 5e-4
-    train_size_data = 2000
+    epochs = 500
+    learning_rate = 1e-4
+    train_size_data = 5000
 
     # load data
     (X_train, y_train), (X_valid, y_valid) = mnist.load_data() # (60000, 28, 28) (60000,) (10000, 28, 28) (10000,)
@@ -33,8 +33,8 @@ def main(args):
     # random data order
     random_order = np.arange(len(X_train))
     np.random.shuffle(random_order)
-    X_train = X_train[random_order][0:train_size_data]
-    y_train = y_train[random_order][0:train_size_data] # FIXME: random label
+    X_train = X_train[0:train_size_data]
+    y_train = y_train[random_order][0:train_size_data] # random label
 
     X_valid = X_valid[0:train_size_data]
     y_valid = y_valid[0:train_size_data]
@@ -48,22 +48,31 @@ def main(args):
         X_placeholder = tf.placeholder(tf.float32, [None, 28, 28, 1])
         Y_placeholder = tf.placeholder(tf.float32, [None, 10])
 
-    predict = simple_model(X_placeholder)
+    logits = simple_model(X_placeholder)
 
     with tf.name_scope("Cross_entropy_loss"):
-        cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y_placeholder * tf.log(predict), reduction_indices=[1]))
+        # cross_entropy = tf.reduce_mean(-tf.reduce_sum(Y_placeholder * tf.log(predict), reduction_indices=[1]))
+        cross_entropy = tf.reduce_mean(
+            tf.nn.softmax_cross_entropy_with_logits_v2(
+                logits=logits, 
+                labels=Y_placeholder
+            )
+        )
         tf.summary.scalar("cross_entropy", cross_entropy) # In tensorboard event
 
     with tf.name_scope("train"):
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
-    accuracy = tf.reduce_mean(
-                    tf.cast(
-                        tf.equal(
-                            tf.argmax(Y_placeholder, dimension= 1),
-                            tf.argmax(predict, dimension= 1)
-                        ), 
-                    tf.float32))
+    with tf.name_scope("accuracy"):
+        accuracy = tf.reduce_mean(
+            tf.cast(
+                tf.equal(
+                    tf.argmax(Y_placeholder, axis=1),
+                    tf.argmax(logits, axis=1)
+                ),
+                tf.float32
+            )
+        )
 
     # train
     with tf.Session() as sess:
@@ -74,10 +83,10 @@ def main(args):
         sess.run(tf.global_variables_initializer())
         nbrof_batch = int(len(X_train) / batch_size)
 
-        train_cross_entropy_epochs = []
-        train_accuracy_epochs = []
-        valid_cross_entropy_epochs = []
-        valid_accuracy_epochs = []
+        train_cross_entropy_list = []
+        train_accuracy_list = []
+        valid_cross_entropy_list = []
+        valid_accuracy_list = []
         for e in range(epochs):
             for i in range(nbrof_batch):
                 batch_x, batch_y = X_train[i * batch_size: (i+1) * batch_size], y_train[i * batch_size: (i+1) * batch_size]
@@ -86,40 +95,36 @@ def main(args):
                 _, summary, train_cross_entropy_ = sess.run([train_step, merged, cross_entropy], feed_dict=feed_dict)
                 
                 train_accuracy_ = sess.run(accuracy, feed_dict=feed_dict)
-                # y_predict_  = sess.run(predict, feed_dict=feed_dict)
-                # correct_prediction_ = tf.equal(tf.argmax(y_predict_, 1), tf.argmax(batch_y, 1))
-                # tensor_accuracy = tf.reduce_mean(tf.cast(correct_prediction_, tf.float32))
-                # accuracy = sess.run(tensor_accuracy, feed_dict=feed_dict)
-
                 train_writer.add_summary(summary, e)
 
                 if i %10 == 0:
                     print("epochs:{}, steps:{}, loss={}, accuracy={}".format(e, (e*nbrof_batch) + i, train_cross_entropy_, train_accuracy_))
             
             # valid model
-            random_choice = np.random.randint(0, len(X_valid), size=batch_size) # FIXME: need check
+            random_choice = np.random.randint(0, len(X_valid), size=batch_size) # need check
             batch_x, batch_y = X_valid[random_choice], y_valid[random_choice]
             feed_dict = {X_placeholder: batch_x, Y_placeholder: batch_y}
 
             valid_accuracy_ = sess.run(accuracy, feed_dict=feed_dict)
             valid_cross_entropy_ = sess.run(cross_entropy, feed_dict=feed_dict)
 
+            print("epochs:{}, steps:{}, valid loss={}, valid accuracy={}".format(e, (e*nbrof_batch) + i, valid_cross_entropy_, valid_accuracy_))
 
-        train_cross_entropy_epochs.append(train_cross_entropy_)
-        train_accuracy_epochs.append(train_accuracy_)
+            train_cross_entropy_list.append(train_cross_entropy_)
+            train_accuracy_list.append(train_accuracy_)
 
-        valid_cross_entropy_epochs.append(valid_cross_entropy_)
-        valid_accuracy_epochs.append(valid_accuracy_)
+            valid_cross_entropy_list.append(valid_cross_entropy_)
+            valid_accuracy_list.append(valid_accuracy_)
 
         # save loss process
-        train_cross_entropy_epochs = np.asarray(train_cross_entropy_epochs)
+        train_cross_entropy_epochs = np.asarray(train_cross_entropy_list)
         train_loss_save_path = os.path.join(args.LOG_DIR_PATH, "Random_label", "train_loss.npy")
         if not os.path.exists(os.path.dirname(train_loss_save_path)):
             os.makedirs(os.path.dirname(train_loss_save_path))
         np.save(train_loss_save_path, train_cross_entropy_epochs)
         print("Train loss process to path: ", train_loss_save_path)
 
-        valid_cross_entropy_epochs = np.asarray(valid_cross_entropy_epochs)
+        valid_cross_entropy_epochs = np.asarray(valid_cross_entropy_list)
         valid_loss_save_path = os.path.join(args.LOG_DIR_PATH, "Random_label", "valid_loss.npy")
         if not os.path.exists(os.path.dirname(valid_loss_save_path)):
             os.makedirs(os.path.dirname(valid_loss_save_path))
@@ -128,14 +133,14 @@ def main(args):
 
 
         # save accuracy process
-        train_accuracy_epochs = np.asarray(train_accuracy_epochs)
+        train_accuracy_epochs = np.asarray(train_accuracy_list)
         train_accuracy_save_path = os.path.join(args.LOG_DIR_PATH, "Random_label", "train_accuracy.npy")
         if not os.path.exists(os.path.dirname(train_accuracy_save_path)):
             os.makedirs(os.path.dirname(train_accuracy_save_path))
         np.save(train_accuracy_save_path, train_accuracy_epochs)
         print("Train accuracy process to path: ", train_accuracy_save_path)
         
-        valid_accuracy_epochs = np.asarray(valid_accuracy_epochs)
+        valid_accuracy_epochs = np.asarray(valid_accuracy_list)
         valid_accuracy_save_path = os.path.join(args.LOG_DIR_PATH, "Random_label", "valid_accuracy.npy")
         if not os.path.exists(os.path.dirname(valid_accuracy_save_path)):
             os.makedirs(os.path.dirname(valid_accuracy_save_path))
