4e36f736e1201c680407f7c149b1502ccff54006
CYC <yenchen0416@gmail.com>
2018-11-13 23:58:15 +0800

    modify model in nbr of para vs gen task

--- HW1-3/model/basic_model.py
+++ HW1-3/model/basic_model.py
@@ -30,7 +30,7 @@ def simple_model(X_placeholder):
 
     output = tf.layers.dense(inputs=dense, units=10
                             , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                            , activation=tf.nn.softmax
+                            , activation=None
                             , name="output")
 
     return output
@@ -39,34 +39,22 @@ def simple_model(X_placeholder):
 
 def similar_model(X_placeholder, time):
 
-    conv = tf.layers.conv2d(inputs=X_placeholder, filters=16
+    conv = tf.layers.conv2d(inputs=X_placeholder, filters=(time+1)*2
                             , kernel_size=3
                             , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
                             , activation=tf.nn.relu
                             , name="conv1")
 
-    conv = tf.layers.conv2d(inputs=conv, filters=32
-                            , kernel_size=3
-                            , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                            , activation=tf.nn.relu
-                            , name="conv2")
-
-    conv = tf.layers.conv2d(inputs=conv, filters=64
-                            , kernel_size=3
-                            , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                            , activation=tf.nn.relu
-                            , name="conv3")
-
     flatten = tf.layers.flatten(conv, name="flatten")
 
-    dense = tf.layers.dense(inputs=flatten, units=(times + 1)*10
-                                , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                                , activation=tf.nn.relu
-                                , name="dense1")
+    dense = tf.layers.dense(inputs=flatten, units=(time+1)*2
+                            , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
+                            , activation=tf.nn.relu
+                            , name="dense1")
 
     output = tf.layers.dense(inputs=dense, units=10
                             , kernel_initializer=tf.truncated_normal_initializer(stddev=0.01)
-                            , activation=tf.nn.softmax
+                            , activation=None
                             , name="output")
 
     return output
